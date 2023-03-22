e36cbabd78f2cfaf79da0cefdfab846c0625fbcb
whoiscaleb <123383563+whoiscaleb@users.noreply.github.com>
2023-03-22 16:56:52 -0400

    Add files via upload
    Added submit button functionality

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
-const query = '10oz steak'
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/nutrition?query=' + query,
-    headers: { 'X-Api-Key': '4nihgxknLnY1MNKXnMOtgw==N2FMBsilvKXpTlXn'},
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-});
 
-$('button').click(function(){
-   $.post(query);
-  });
+function handleGetData(event) {
+    event.preventDefault();
+    const query = $('#query').val();
+    $.ajax({
+        method: 'GET',
+        url: 'https://api.api-ninjas.com/v1/nutrition?query=' + query,
+        headers: { 'X-Api-Key': '4nihgxknLnY1MNKXnMOtgw==N2FMBsilvKXpTlXn'},
+        contentType: 'application/json',
+        success: function(result) {
+            console.log(result);
+        },
+        error: function ajaxError(jqXHR) {
+            console.error('Error: ', jqXHR.responseText);
+        }
+    });
+}
+
+$('form').on('submit', handleGetData);
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
 
 
 
